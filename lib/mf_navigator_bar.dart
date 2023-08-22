b9226459e93866e5e7e38f7e3c594474886a304f
machengyong <chengyong.ma1@pactera.com>
2023-08-22 13:35:21 +0800

    FIX: 优化代码

--- lib/mf_navigator_bar.dart
+++ lib/mf_navigator_bar.dart
@@ -44,105 +44,80 @@ class _MFNavigatorBarState extends State<MFNavigatorBar> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 14),
-                child: Container(
-                  height: 60,
-                  color: Colors.white,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'tab1',
-                    style: TextStyle(fontSize: 16, color: Colors.blue),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 14),
-                child: Container(
-                  height: 60,
-                  color: Colors.white,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'tab1',
-                    style: TextStyle(fontSize: 16, color: Colors.blue),
-                  ),
-                ),
-              ),
-              GestureDetector(
-                child: Container(
-                  width: 60,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    gradient: const LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Color(0xFF005CBF),
-                        Color(0xFF004EA1),
-                      ],
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black.withOpacity(0.3),
-                          offset: const Offset(0, 4),
-                          blurRadius: 8,
-                          spreadRadius: 1.0),
-                    ],
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Icon(
-                        Icons.add,
-                        color: Colors.white,
-                      ),
-                      Container(
-                        alignment: Alignment.center,
-                        margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                        child: const Text(
-                          '1111',
-                          maxLines: 1,
-                          style: TextStyle(
-                            fontSize: 9.0,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                onTap: () {},
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 14),
-                child: Container(
-                  height: 60,
-                  color: Colors.white,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'tab1',
-                    style: TextStyle(fontSize: 16, color: Colors.blue),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 14),
-                child: Container(
-                  height: 60,
-                  color: Colors.white,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'tab1',
-                    style: TextStyle(fontSize: 16, color: Colors.blue),
-                  ),
-                ),
-              ),
+              _normalTab('Tab1'),
+              _normalTab('Tab2'),
+              _centerTab(),
+              _normalTab('Tab3'),
+              _normalTab('Tab4'),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  /// 正常tab
+  Widget _normalTab(String tabName) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 14),
+      child: Container(
+        height: 60,
+        color: Colors.white,
+        alignment: Alignment.center,
+        child: Text(
+          tabName,
+          style: const TextStyle(fontSize: 16, color: Colors.blue),
+        ),
+      ),
+    );
+  }
+
+  /// 中间圆形tab
+  Widget _centerTab() {
+    return GestureDetector(
+      child: Container(
+        margin: const EdgeInsets.only(top: 1),
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: const LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF005CBF),
+              Color(0xFF004EA1),
+            ],
+          ),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.3), offset: const Offset(0, 4), blurRadius: 8, spreadRadius: 1.0),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              child: const Text(
+                '1111',
+                maxLines: 1,
+                style: TextStyle(
+                  fontSize: 9.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      onTap: () {},
     );
   }
 }
